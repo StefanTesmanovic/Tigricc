0a0ede6210c5d82dce1a976d89d946331ab2c8d7
StefanTesmanovic <83782548+StefanTesmanovic@users.noreply.github.com>
2023-11-12 21:29:45 +0100

    Tigrilijanovic

--- tigric/assets/js/script.js
+++ tigric/assets/js/script.js
@@ -44,3 +44,66 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 50 ? header.classList.add("active")
     : header.classList.remove("active");
 });
+
+function openGallery(evt, igraonica) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("Galerija");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(igraonica).style.display = "block";
+}
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+    
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}      
+function showSlides1(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  if (n > slides.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex1-1].style.display = "block"; 
+  }
+
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+    
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}      
+function showSlides3(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides3");
+  if (n > slides.length) {slideIndex3 = 1}    
+  if (n < 1) {slideIndex3 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex3-1].style.display = "block"; 
+}
+
+
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+    
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}      
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  if (n > slides.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[slideIndex2-1].style.display = "block"; 
+  }
+
